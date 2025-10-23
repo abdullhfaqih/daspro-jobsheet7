09f5886b27529bdfc675ca2ef85a1881436b8823
abdullah faqih <faqih7665@gmail.com>
2025-10-23 11:22:04 +0700

    Modifikasi Program

--- SiakadFor01.java
+++ SiakadFor01.java
@@ -4,6 +4,7 @@ public class SiakadFor01 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     double nilai, tertinggi = 0, terendah = 100;
+    int lulus = 0, tidakLulus = 0;
 
     for (int i = 1; i <= 10; i++) {
       System.out.print("Masukkan nilai ke-" + i + ": ");
@@ -14,10 +15,19 @@ public class SiakadFor01 {
       if (nilai < terendah) {
         terendah = nilai;
       }
+
+      // Modifikasi untuk menentukan lulus atau tidak
+      if (nilai >= 60) {
+        lulus++;
+      } else {
+        tidakLulus++;
+      }
     }
 
     System.out.println("Nilai tertinggi: " + tertinggi);
     System.out.println("Nilai terendah: " + terendah);
+    System.out.println("Jumlah mahasiswa lulus: " + lulus);
+    System.out.println("Jumlah mahasiswa tidak lulus: " + tidakLulus);
 
     input.close();
   }
